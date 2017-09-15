3121eb64a0c16ae67ebf6300e31b913c24ac4d16
Linus Törngren <linus@etnolit.se>
2017-09-15 15:21:03 +0200

    Proof of concept for game map.

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 from flask import Flask, session, render_template
+from random import random
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Something very secret and hard to guess!'
 
+BOMB_PROBABILITY = 20
+
+def generate_map():
+    m = [[random() < 1/BOMB_PROBABILITY for x in range(20)] for y in range(10)]
+
+    # Same as above double list comprehension.
+    #
+    # m = []
+    # for y in range(10):
+    #     row = []
+    #     for x in range(20):
+    #         row.append( random() < 1/BOMB_PROBABILITY )
+    #     m.append(row)
+
+    return m
+
 
 @app.route("/")
 def index():
-    if session.get('minne'):
-        text = 'Hej, jag känner igen dig.'
-    else:
-        session['minne'] = True
-        text = 'Hej!'
+    if not session.get('map'):
+        session['map'] = generate_map()
 
-    return render_template('index.html', text=text)
+    return render_template('index.html', map=session['map'])
 
 
 if __name__ == "__main__":
